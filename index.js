66be1555e53e3deb2fd086e80e6a7bb76ffd9649
Peter <rytko@ii.uj.edu.pl>
2016-07-20 11:11:33 +0200

    serving updates through different server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 const fs = require('fs');
-const fsU = require('nodejs-fs-utils');
 const path = require('path');
 const express = require('express');
 const http = require('http');
 const https = require('https');
+const stream = require('stream');
 const app = express();
 const packageJSON = require('./package.json');
 const minidump = require('minidump');
@@ -17,107 +17,85 @@ app.use(require('morgan')('dev'));
 app.use('/updates/releases', express.static(path.join(__dirname, 'releases')));
 app.use(require('connect-busboy')());
 
-let getBaseUrl = () => {
-    return (PROD ? packageJSON.productionURL : 'http://localhost')+((PORT && (parseInt(PORT) > 0)) ? ':'+PORT : '');
+let getFilesBaseUrl = () => {
+    return PROD ? packageJSON.productionURL : 'http://localhost'+((PORT && (parseInt(PORT) > 0)) ? ':'+PORT : '');
 };
 let sendError = (res, error) => {
     res.set('Content-Type', 'text/html');
     res.send('<h1 style="color:#a00;text-align:center;">Sorry, an ERROR occured</h1><small style="color:#eb0000;"><pre>'+error+'</pre></small>');
 };
-let formatSize = size => {
-    let out = size;
-    let level = -1;
-    while(out / 1000 > 0){
-        out = Math.floor(out / 1000);
-        ++level;
-    }
-    out = Math.floor(size / Math.pow(10,level*3));
-    switch(level){
-        case 0: return out+" B";
-        case 1: return out+" KB";
-        case 2: return out+" MB";
-        case 3: return out+" GB";
-        default: return size+" B";
+let createLatestResponse = (req, res, qres) => {
+    const clientVersion = req.query.v;
+    const platform = req.query.p || 'darwin';
+    const arch = req.query.a || 'x64';
+    const latest = qres.latest || qres;
+    if(clientVersion === latest){
+        res.status(204).end();
+    }else{
+        platform == 'darwin' ?
+            res.json({ url : qres.url || `${getFilesBaseUrl()}/darwin/${latest}/${packageJSON.zipFilename}.zip` }) :
+            res.json({ url : `${getFilesBaseUrl()}/win32/${arch}` })
     }
 };
-let getPlatform = fname => {
-    switch(fname.replace(/(^.+)(\.[^.]+$)/,"$2")){
-        case ".exe":
-            let arch = fname.match(/(x64)|(ia32)/) ? fname.match(/(x64)/) ? " - 64 bits" : " - 32 bits" : "";
-            return "Windows"+arch;
-        case ".app": return "Mac OSX";
-        default: return "&nbsp;";
-    }
+let createFilesTable = (res, message) => {
+    getFileContent(getFilesBaseUrl()+'/files.json')
+        .then(content => {
+            let page = "<h1>Files to download</h1>\n";
+            page += content.downloads.length ?
+                "<h4>Chose a file to download</h4>\n<table>\n\t<tr><th>Filename</th><th>Size</th><th>Last modified date</th><th>Platform</th><th>&nbsp;</th></tr>" :
+                "<h4>No files do download</h4>\n";
+            content.downloads.forEach(file => {
+                let lm = new Date(file.lastModified);
+                page += "\t<tr>\n";
+                page += "\t\t<td>"+file.fileName+"</td>\n";
+                page += "\t\t<td>"+file.size+"</td>\n";
+                page += "\t\t<td>"+lm.toLocaleDateString()+" "+lm.toLocaleTimeString()+"</td>\n";
+                page += "\t\t<td>"+file.platform+"</td>\n";
+                page += "\t\t<td>\n";
+                page += "\t\t\t<form method=\"GET\">\n";
+                page += "\t\t\t\t<input type='submit' value='Download' class='btn btn-success' />\n";
+                page += "\t\t\t\t<input type='hidden' value='"+file.fileName+"' name='f' />\n";
+                page += "\t\t\t</form>\n";
+                page += "\t\t</td>\n";
+                page += "\t</tr>\n";
+            });
+            page += content.downloads.length ? "</table>\n" : "";
+            res.set('Content-Type', 'text/html');
+            fs.readFile(path.resolve(__dirname,'download.html'), function (err, content) {
+                if(err) return sendError(res,err);
+                return res.send(content.toString().replace('#page#', page).replace('#message#', message || ''));
+            });
+        }, e => sendError(res,e))
+        .catch(e => sendError(res,`ERROR: ${e}`));
 };
 
 app.get('/download',(req,res)=>{
-
-    getFileContent('http://localhost:3333/updates/latest')
-        .then(r => console.log(JSON.stringify(JSON.parse(r),null,4)))
-        .catch(e => console.error(JSON.stringify(e,null,4)));
-
     let fileName = req.query.f;
-    let filePath = path.resolve(__dirname,'releases','download',fileName || '.');
-    if(fileName && path.basename(filePath) == fileName)
-        return res.download(filePath);
-
-    // res.setHeader('Content-disposition', 'attachment; filename='+fileName);
-    // var filestream = fs.createReadStream(filePath);
-    // filestream.pipe(res);
-    let dir = path.resolve(__dirname,'releases','download');
-    fs.readdir(dir,(err,files)=>{
-        if(err) return sendError(res,err);
-        let filteredFiles = files.filter((file) => {
-            const fp = path.join(filePath, file);
-            return !file.match(/^\./);
-        });
-        let page = "<h1>Files to download</h1>\n";
-        page += filteredFiles.length ?
-            "<h4>Chose a file to download</h4>\n<table>\n\t<tr><th>Filename</th><th>Size</th><th>Last modified date</th><th>Platform</th><th>&nbsp;</th></tr>" :
-            "<h4>No files do download</h4>\n";
-        filteredFiles.forEach(file=>{
-            let stats = fs.statSync(path.join(dir,file));
-            let size = stats.isDirectory() ? fsU.fsizeSync(path.join(dir,file)) : stats.size;
-            page += "\t<tr>\n";
-            page += "\t\t<td>"+file+"</td>\n";
-            page += "\t\t<td>"+formatSize(size)+"</td>\n";
-            page += "\t\t<td>"+stats.mtime.toLocaleDateString()+" "+stats.mtime.toLocaleTimeString()+"</td>\n";
-            page += "\t\t<td>"+getPlatform(file)+"</td>\n";
-            page += "\t\t<td>\n";
-            page += "\t\t\t<form method=\"GET\">\n";
-            page += "\t\t\t\t<input type='submit' value='Download' class='btn btn-success' />\n";
-            page += "\t\t\t\t<input type='hidden' value='"+file+"' name='f' />\n";
-            page += "\t\t\t</form>\n";
-            page += "\t\t</td>\n";
-            page += "\t</tr>\n";
-        });
-        page += filteredFiles.length ? "</table>\n" : "";
-        res.set('Content-Type', 'text/html');
-
-
-        fs.readFile(path.resolve(__dirname,'download.html'), function (err, content) {
-            if(err) return sendError(res,err);
-            return res.send(content.toString().replace('#page#', page));
-        });
-    });
+    if(fileName){
+        // return getFileContent(getFilesBaseUrl()+"/"+fileName,res,fileName)
+        //     .then(null,e => createFilesTable(res,e)).catch(e => createFilesTable(res,`ERROR: ${e}`));
+        return res.redirect(getFilesBaseUrl()+"/"+fileName);
+    }
+    createFilesTable(res);
 });
 app.get('/info', (req, res) => {
     res.json({ 
         productionUrl : `${packageJSON.productionURL}`, 
         port : `${PORT}`, 
-        baseUrl : `${getBaseUrl()}`
+        baseUrl : `${getFilesBaseUrl()}`
     });
 });
 app.get('/updates/latest', (req, res) => {
-    const clientVersion = req.query.v;
     const platform = req.query.p || 'darwin';
     const arch = req.query.a || 'x64';
-    const latest = getLatestRelease(platform,clientVersion,arch);
-    if(clientVersion === latest){
-        res.status(204).end();
-    }else{
-        res.json({ url : `${getBaseUrl()}/releases/darwin/${latest}/${packageJSON.zipFilename}.zip` });
+    if(platform == 'darwin'){
+        return getFileContent(getFilesBaseUrl()+"/darwin/latest")
+            .then(latest => createLatestResponse(req,res,latest), e => res.status(404).end())
+            .catch(e => res.status(404).end());
     }
+    getFileContent(getFilesBaseUrl()+"/win32/"+arch+"/RELEASES")
+        .then(content => createLatestResponse(req,res,content.split("\n").reverse()[0].split(' ')[1].replace(/^([a-zA-z]+-)(([0-9]+(\.)?)+)(-full\.nupkg)$/,"$2")), e => res.status(404).end())
+        .catch(e => res.status(404).end());
 });
 app.post('/viewReport',bodyParser.urlencoded({ extended: false }),(req,res)=>{
     let filename = req.body.dump;
@@ -163,8 +141,8 @@ app.get('/viewReport',(req,res)=>{
 
 app.post('/crashReporter',bodyParser.urlencoded({ extended: false }),(req, res) => {
     var savePath;
-    console.log(JSON.stringify(req.body,null,2));
-    console.log('guid: '+req.body.guid);
+    // console.log(JSON.stringify(req.body,null,2));
+    // console.log('guid: '+req.body.guid);
     req.busboy.on('file',(fieldname, file, filename, encoding, mimetype)=>{
         savePath = path.join(__dirname,'dumps',filename);
         file.pipe(fs.createWriteStream(savePath));
@@ -178,30 +156,23 @@ app.post('/crashReporter',bodyParser.urlencoded({ extended: false }),(req, res) 
     return req.pipe(req.busboy);
 });
 
-let getLatestRelease = (platform, version, arch) => {
-    const dir = path.resolve(__dirname,'releases',platform);
-
-    if(platform == 'darwin'){
-        return fs.readdirSync(dir).filter((file) => {
-            const filePath = path.join(dir, file);
-            return fs.statSync(filePath).isDirectory();
-        }).reverse()[0] || version;
-    }
-    const filename = path.join(dir,arch,'RELEASES');
-    if(!fs.existsSync(filename)) return version;
-    return fs.readFileSync(filename,'utf8','r').split("\n").reverse()[0].split(' ')[1].replace(/^([a-zA-z]+-)(([0-9]+(\.)?)+)(-full\.nupkg)$/,"$2");
-};
-let getFileContent = fileAddress => {
-    console.log('reading '+fileAddress);
+let getFileContent = (fileAddress, response, fileName) => {
     return new Promise((success, error) => {
         (fileAddress.match(/^https/) ? https : http).get(fileAddress, res => {
+            let isBuffer = response && res.statusCode < 400;
             let myBuff = '';
-            res.setEncoding('utf8').on('data', d => myBuff += d);
-            res.on('end',() => success(myBuff));
-        }).on('error', e => {
-            console.log(`Got error: ${e.message}`);
-            error(e);
-        });
+            let isJSON = !isBuffer && res.headers['content-type'].match(/json/i);
+            if(isBuffer){
+                res.setHeader('Content-disposition', 'attachment; filename='+fileName);
+                res.pipe(response);
+            }else{
+                let charset = res.headers['content-type'].match(/(^.*charset=)(.+$)/) ? (res.headers['content-type'].replace(/(^.*charset=)(.+$)/, "$2") || 'utf-8').replace('-', '') : 'utf8';
+                res.setEncoding(charset).on('data', d => myBuff += d);
+                res.on('end', () => res.statusCode > 399 ?
+                    error(`ERROR ${res.statusCode}: ${myBuff}`) :
+                    success(isJSON ? JSON.parse(myBuff) : myBuff));
+            }
+        }).on('error', e => error(e.message));
     });
 };
 
